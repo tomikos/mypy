723ca398feca1208145e69ab9d23afacddb17f06
Tomer <tomer123man@gmail.com>
2014-07-16 14:12:25 -0700

    more to do

--- multiSshExec.py
+++ multiSshExec.py
@@ -7,19 +7,22 @@
 import os, socket, subprocess
 from sys import argv, stdout
 from time import sleep, time
-from threading import Thread
-from optparse import OptionParser, OptionGroup
-from optparse import Option, OptionValueError
-from Queue import Queue
+import threading
+from optparse import Option, OptionParser, OptionGroup
 
 try:
 	import libssh2
 except ImportError:
-	print '\n%s: Please install Pylibssh2 module.' % argv[0]
-	os._exit(1)
+	print '\n[-] %s: Please install Pylibssh2 module.' % argv[0]
+	exit(1)
 	
 
 ############################
+
+# Print total running duration
+def totaltime():
+	totaltime = int(time() - starttime)
+	myprint('\n[+] Done - Total duration time: %s' % totaltime)
 
 # Print to stdout if not quiet
 def myprint(msg):
@@ -45,8 +48,7 @@ def printlog(host, msg):
 def printerrlog(host, msg):
 
 	with open(opts.errfile, 'a') as Errfile:
-		Errfile.write('%s:\n' % host)
-		Errfile.write(str(msg) + "\n")
+		Errfile.write(host + ': ' + str(msg) + "\n")
 		Errfile.write('------------------------' + "\n")
 
 # Remove empty logs
@@ -79,29 +81,32 @@ def checkfile(file, oper):
                 msg = 'write to'
 
         try:
-                op_file = open(file, oper)
-                #op_file.close()
+
+		with open(file, oper) as op_file:
+			pass
 
         except IOError:
-                myprint('\n%s: Unable to %s file \'%s\'\n' % (argv[0], msg, file))
-		os._exit(1)
+                myprint('\n[-] %s: Unable to %s file \'%s\'\n' % (argv[0], msg, file))
+		exit(1)
 
         except Exception, e:
-                myprint('\n%s: Unknown error occurred while try to %s file \'%s\'\n' % (argv[0], msg, file))
-		os._exit(1)
+                myprint('\n[-] %s: Unknown error occurred while try to %s file \'%s\'\n' % (argv[0], msg, file))
+		exit(1)
 
 # Execute post script if needed
 def postscript():
 	if opts.post:
 		if os.access(opts.post, os.X_OK):
-			myprint('\nRunning post script: \'%s\'' % opts.post)
+			myprint('\n[+] Running post script: \'%s\'' % opts.post)
 
 			if '/' not in opts.post:
 				subprocess.call('./'+opts.post, shell=True)
 			else:
 				subprocess.call(opts.post, shell=True)
 		else:
-			myprint('\nUnable to execute post script \'%s\'' % opts.post)
+			myprint('\n[-] Unable to execute post script \'%s\'' % opts.post)
+
+# Check platform
 def platform(opts):
 
 	# 'No Linux' is stronger than 'linux versions'
@@ -114,7 +119,7 @@ def platform(opts):
 	# No Linux and no HPUX
 	if (not opts.linux) and (not opts.linuxver) and (not opts.hpux):
 		myprint('\nNo paltform choosed to ruuning on')
-		os._exit(0)
+		exit(0)
 
 	# Only HPUX 
 	if not (opts.linuxver and opts.linux) and (opts.hpux):
@@ -212,11 +217,11 @@ def multiTarget(opts):
 
 	# Check if hostfile is empty
 	if opts.totalhosts == 0:
-		myprint('\nThe hostfile: \'%s\' is empty!' % opts.hostfile)
+		myprint('\n[-] The hostfile: \'%s\' is empty!' % opts.hostfile)
 		exit(1)
 
 	# Determine chosen platform
-	opts.platcheck = platform(opts)
+	#opts.platcheck = platform(opts)
 
 	# Redefine threads number if it greater then the total of hosts
 	if opts.totalhosts < opts.threads:
@@ -224,11 +229,11 @@ def multiTarget(opts):
 
 	# Reset needed variables
 	results = []
-	opts.tcounter = 0
 	opts.cun = 0
+	opts.tcounter = 0
 	opts.hostname = ''
 
-
+	# Open hostlist for reading
 	op_hostlist = open(opts.hostfile, 'r')
 
 	# Run on every host in hostfile
@@ -237,33 +242,34 @@ def multiTarget(opts):
 		opts.cun += 1
 		sleep(0.2)
 
-		print "[%s/%s] Connecting to: %s " % (opts.cun, opts.totalhosts, opts.hostname),
-
 		try:
 			opts.t = SshExec(opts)
 			results.append(opts.t)
 			opts.t.start()
-			opts.t.daemon=True
+			#opts.t.daemon=True
 			opts.tcounter += 1
 
 		except Exception, e:
 			continue
-	
+
+		# Wait for thread series to finish
 		if opts.tcounter == opts.threads:
 			for result in results:
 				result.join()
 			opts.tcounter = 0
 
+			# Delay time setted
+			sleep(opts.delay)
+
 	op_hostlist.close()
 
 # pylibssh2 with Thread
-class SshExec(Thread):
+class SshExec(threading.Thread):
         def __init__(self, opts):
                 super(SshExec, self).__init__()
 
                 # Define needed variables
                 self.Hostname = opts.hostname
-		self.Target = opts.target
 		self.Cmd = opts.cmd
 		self.Script = opts.script
 		self.User = opts.user
@@ -271,44 +277,88 @@ class SshExec(Thread):
 		self.Port = opts.port
 		self.Key = opts.key
 		self.Pkey = opts.pkey
+		self.Cun = opts.cun
 		self.Totalhosts = opts.totalhosts
-		self.session = libssh2.Session()
 
-		# Prepare socket connection with libssh2
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.setblocking(1)
-		self.sock.settimeout(self.Timeout)
-
-	# Run the execution!
-	def run(self):
+	# Connect to host
+	def conn(self):
 		try:
-			#def connect(self):
+			self.session = libssh2.Session()
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.setblocking(1)
+			self.sock.settimeout(self.Timeout)
 			self.sock.connect((self.Hostname, self.Port))
 			self.session.startup(self.sock)
-			sleep(0.2)
 			self.session.userauth_publickey_fromfile(self.User, self.Pkey, self.Key, '')
-	  		self.channel = self.session.open_session()
-
+  			self.channel = self.session.open_session()
 
 		except Exception, e:
-			myprint("- Failed!")
+			myprint("[%s/%s] Connecting to: %s - Failed!" % (self.Cun, self.Totalhosts, self.Hostname))
 			printerrlog(self.Hostname, e)
+			exit(0)
 
-			raise Exception
+	# Execute command on remote host
+	def exe(self):
+		try:
+			# Excute command on the remote host
+       			self.channel.execute(self.Cmd)
 
+		except Exception, e:
+			printerrlog(self.Hostname, e)
+			exit(0)
 
+		data = self.channel.read(4096)
+
+       		#print self.channel.exit_status()
+
+		try:
+			# Close the channel
+	        	self.channel.close()
+			self.session.close()
+
+		except Exception, e:
+			printerrlog(self.Hostname, e)
+			exit(0)
+
+		return data
+
+	# Start running!
+	def run(self):
+
+		self.conn()
+		output = self.exe()
+
+		if output == '' or output is None:
+			pass
+		else:
+			#printlog(self.Hostname, output.strip())
+			print output.strip()
+
+		self.conn()
+		output = self.exe()
+
+		if output == '' or output is None:
+			pass
+		else:
+			#printlog(self.Hostname, output.strip())
+			print output.strip()
+
+		"""
 		# If using script
                 if not self.Cmd:
                         # Load local script into variable
                         with open(self.Script, "r") as myfile:
                                 self.Cmd=myfile.read()
 
-
 		# Put the pre check platform at the top of the command
 		#self.Cmd = opts.platcheck + self.Cmd
 
-		# Excute command on the remote host
-       		self.channel.execute(self.Cmd)
+		try:
+			# Excute command on the remote host
+       			self.channel.execute(self.Cmd)
+
+		except Exception, e:
+			printerrlog(self.Hostname, e)
 
 		while True:
 			data = self.channel.read(4096)
@@ -317,25 +367,23 @@ class SshExec(Thread):
 			else:
 				printlog(self.Hostname, data.strip())
 
-		#print self.channel.wait_closed()
        		#print self.channel.exit_status()
-		#print self.channel.eof()
 
 		try:
 			# Close the channel
 	        	self.channel.close()
-			myprint("- Sucsess!")
+			myprint("[%s/%s] Connecting to: %s - Sucsess!" % (self.Cun, self.Totalhosts, self.Hostname))
 
 		except Exception, e:
-			myprint("- Sucsess with error! ")
+			myprint("[%s/%s] Connecting to: %s - Sucsess with error!" % (self.Cun, self.Totalhosts, self.Hostname))
 			printerrlog(self.Hostname, e)
-
-	def __del__(self):
+		"""
+	#def __del__(self):
 		# Close the connection
 		self.session.close()
 
 # Setup the parser
-usage = "%prog [HOST] [COMMAND] [OPTIONAL]..."
+usage = "%prog [HOST] [COMMAND] [OPTIONS].."
 
 description = '''Example: %prog -t Myhost -t Otherhost -c "uname -a" -T 20	
 Example: %prog -H host.lst -S script.sh -w 5 -O results.log'''
@@ -352,7 +400,7 @@ parser.add_option("-l", "--verlog",
         action='store_true',
 	default=False,
 	dest='verboselog',
-        help='Verbose log (default: %default)')
+        help='Verbose logging (default: %default)')
 parser.add_option("-q", "--quiet",
         action='store_true',
 	default=False,
@@ -380,27 +428,32 @@ group1.add_option("-S",
 	type='string',
         help="Script to execute on remote host")
 
-group2 = OptionGroup(parser, "Optional")
+group2 = OptionGroup(parser, "Advanced")
 group2.add_option("-T",
         metavar="<THREADS>", dest="threads",
 	type='int',
 	default=10,
         help="Number of connections in parallel (default: %default)")
-group2.add_option("-p",
-        metavar="<PORT>", dest="port",
+group2.add_option("-d",
+        metavar="<DELAY>", dest="delay",
 	type='int',
-	default=22,
-        help="SSH port number (default: %default)")
+	default=0,
+        help="Delay time between thread series (default: %default sec)")
 group2.add_option("-w",
         metavar="<TIMEOUT>", dest="timeout",
 	type='int',
 	default=15,
-        help="Max wait time in seconds for responses (default: %default sec)")
+        help="Max wait time in seconds for response (default: %default sec)")
 group2.add_option("-u",
         metavar="<USER>", dest="user",
 	type='string',
 	default='root',
         help="Username to connect with (default: %default)")
+group2.add_option("-p",
+        metavar="<PORT>", dest="port",
+	type='int',
+	default=22,
+        help="SSH port number (default: %default)")
 
 group3 = OptionGroup(parser, "Platforms")
 group3.add_option("-N",
@@ -458,14 +511,11 @@ starttime = time()
 # Grab the command line options
 (opts, args) = parser.parse_args()
 
-
-Kinterrupt=False
-
 ############ Pre Checking Section ############
 # Check for required options
 if len(argv) < 2:
 	parser.print_usage()
-	os._exit(0)
+	exit(0)
 
 # Check for required options -c / -S
 if (not opts.cmd and not opts.script):
@@ -473,57 +523,63 @@ if (not opts.cmd and not opts.script):
 elif (opts.cmd and opts.script):
 	parser.error("Only command or script can be handle")
 elif (opts.script and not opts.cmd):
-	checkfile(opt.script, 'r')
+	checkfile(opts.script, 'r')
 
 # Check for required options -t / -H
 if (not opts.target and not opts.hostfile):	
 	parser.error("Missing hostname or hostlist")
 elif (opts.target and opts.hostfile):
-	parser.error("Only hostname or host list can be handle")
+	parser.error("Only hostname or hostlist can be handle")
 
 ######### Calling Function Section ##########
-# Start!
+# -Start-
 try: 
 	# Check for needed files
 	checkfile(opts.outfile, 'w')
 	checkfile(opts.errfile, 'w')
 
-	# Start function depend if choose multihost or not
+	# Start function depend if choose multihost or single
 	if (opts.target and not opts.hostfile):
 		singelTarget(opts)
 	elif (opts.hostfile and not opts.target):
 		multiTarget(opts)
 
-        # Wating for all threads to finish
-        while True:
-                sleep(0.1)
-                if not opts.t.isAlive():
-                        break
-
 # Handle user Ctrl+C keyboard
 except KeyboardInterrupt, e:
-	myprint("\nStopped by user!")
-	
-	# Remove empty created logs
-	removelog()
 
-	interrupt=True
+        # Wating for all threads to finish
+	if threading.active_count() > 1:
+		sleep(2)
+		myprint("\n[!] Stopped by user - Waiting for lasts threads to finish ..\n")
 
-	# Exit with error
-	os._exit(1)
-
-# End!
-finally:
-	if not Kinterrupt:
-		# Execute post script if needed
-		postscript()
+		while threading.active_count() > 1:
+			pass
+	else:
+		myprint("\n[!] Stopped by user!")
 
 	# Remove empty created logs
 	removelog()
 
 	# Print total running duration
-	totaltime = int(time() - starttime)
-	myprint('\nDone - Total Time Taken: %s' % totaltime)
+	totaltime()
 
-	# Exit
-	os._exit(0)
+	# Exit with error
+	exit(10)
+
+# -Finish-
+
+# Wating for all threads to finish
+while threading.active_count() > 1:
+	pass
+
+# Execute post script if needed
+postscript()
+
+# Remove empty created logs
+removelog()
+
+# Print total running duration
+totaltime()
+
+# Exit
+exit(0)
